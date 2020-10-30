4ecd3f58432a56b6448a57dad0dd96f5ba6b22aa
Tanausu Suarez <tsuarezm@itccanarias.org>
2020-10-30 18:36:23 +0000

    Nuevos ejercicio Biblioteca Array

--- src/bloque_I/BibliotecaArray.java
+++ src/bloque_I/BibliotecaArray.java
@@ -148,6 +148,42 @@ public class BibliotecaArray {
 		return total;
 	}
 	
+	/*EJERCICIOS PROPUESTO*/
+	
+	/*
+	 * crea un método que elimine un elemento de una posición dada de un array
+	 * */
+	
+	/*
+	 * Crea un método que ordene los elemento de un array. Tendrá un parámetro boolean, si este es true, lo ordenará de mayor a menor, si es falso
+	 * de menor a mayor
+	 * */
+	
+	/*
+	 * crea un metodo que guarde dentro de un array un elemento en la posición que se le indica. Si la izquierda no hay valores se rellena con 0
+	 * */
+	
+	/*
+	 * Crea un método que devuelva las letras del DNI. Las letras del DNI  se obtiene como el resto de dividir el número del DNI entre 23,
+	 * Por ejemplo, si introducimos el  DNI 20267079, el resto de dividirlo por 23 sería 8, luego la letra sería la P  
+	 * 
+	 * Posición     0   1   2   3	4	5   6	7	8	9	10	11  12	13  14  15  16  17  18  19  20  21  22
+	 * Letra        T   R  	W 	A	G	M	Y	F	P	D	X	B	N   J    Z   S   Q   V   H   L   C   K   E
+	 * */
+	
+	/*
+	 * Crea un método que compare dos array y que devuelva true si son iguales o false si no lo son
+	 * */
+	
+	/*
+	 * Crea un método que devuelva una sub-array de un array dado. El método recibirá además dos parámetros, que determine el principio y el fin de los valores que
+	 * debe devolver el método
+	 * Por ejemplo:
+	 *   array = [2, 3, 4, 5, 6, 8] ini = 1, fin = 3
+	 *   array resultado = [3, 4, 5]
+	 */
+	 
+	
 }
 
 
