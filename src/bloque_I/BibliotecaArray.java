13e6e85d70bd60d594f413e2b3031d7646980100
manupe13 <manupemen13@gmail.com>
2020-10-30 13:53:28 +0000

    ejercicios de BibliotecaArray terminados

--- src/bloque_I/BibliotecaArray.java
+++ src/bloque_I/BibliotecaArray.java
@@ -2,7 +2,8 @@ package bloque_I;
 
 public class BibliotecaArray {
 	
-	//Crear un metodo que duevuelva true si el valor pasado por parametro 
+	//Crear un metodo que duevuelva true si el valor pasado por parametro esta en el array
+	
 	public static boolean encontrarString(String [] lista, String valor) {
 		
 		for(int i = 0; i < lista.length; i++) {
@@ -13,53 +14,139 @@ public class BibliotecaArray {
 		return false;
 	}
 	
-	//Crear un metodo pasando por parametro un array de tipo String y un elemento de tipo de String  e insertamos el elemento dentro del Array por el principio
+	//********************************************************************************************************************************************//
 	
-	//lista = {juan, pepe, lucas}
+	//Crear un metodo pasando por parametro un array de tipo String y un elemento de tipo de String e insertamos el elemento dentro del Array por el principio
+	
+	//lista = {pepe, juan, lucas} tamaño 3
 	//elemento = ana
 	//listaResult = {null, null, null, null}
-	
-	
 	
 	public static String [] añadirPrincipio(String [] lista, String elemento) {
 		
 		String [] listaResult = new String [lista.length +1];
 	
 		for(int i = 0; i < lista.length; i++) {
-			listaResult [i+1] = lista[i];	
+			listaResult[i+1] = lista[i];	
 		}
-		
-		listaResult [0] = elemento;
+		listaResult[0] = elemento;
 		
 		return listaResult;
 	}
 	
-	//lista = {juan, pepe, lucas}
+	//********************************************************************************************************************************************//
+	
+	//lista = {pepe, juan, lucas} tamaño 3
 	//elemento = ana
 	//listaResult = {null, null, null, null}
 	
 	public static String [] añadirFinal(String [] lista, String elemento) {
 		
-		String [] listaResult = new String[lista.length+1];
+		String [] listaResult = new String [lista.length +1];
 		
 		for(int i = 0; i < lista.length; i++){
-			listaResult [i] = lista [i];
+			listaResult[i] = lista[i];
 		}
-		listaResult[listaResult.length-1] = elemento;
+		listaResult[listaResult.length -1] = elemento;
 		
 		return listaResult;
 	}
 	
+	//********************************************************************************************************************************************//
 	
 	//Borra un elemento pasado por parametro dentro de un array (array, elemento)
 	
+	//Borrar primer elemento de la lista
+	//          0     1     2
+	//lista = {pepe, juan, lucas} tamaño 3
+	//elemento = juan
+	//si esta
+	//                0     1
+	//listaResult = {null, null, } tamaño 2
+	//si no esta
+	//elemento = ana
+	//listaResult = {null, null, null } tamaño 3
+	
+	
+	
+	public static String [] eliminarElemento(String [] lista, String elemento) {
+		/*
+		int tamaño = 0;
+		
+		for(int i = 0; i < lista.length; i++) {
+			if(elemento != lista[i]) {
+				tamaño ++;
+				
+			} else if(elemento == lista[i]) {
+				tamaño ++;
+			}
+			
+		}
+		*/
+		//                                  tamaño
+		String [] listaResult = new String [lista.length];
+		
+		for(int i = 0; i < lista.length; i++) {
+			if(elemento != lista[i]) {
+				listaResult[i]=lista[i];
+				
+			} else if(elemento == lista[i]) {
+				listaResult[i+1]=lista[i];
+			}
+			
+		}
+		
+		return listaResult;
+		
+	}
+	
+	//********************************************************************************************************************************************//
 	
 	//Borrar primer elemento de la lista
+	//          0     1     2
+	//lista = {pepe, juan, lucas} tamaño 3
+	//                0     1
+	//listaResult = {null, null, } tamaño 2
+	
+	public static String [] eliminarPrimero(String [] lista) {
+		
+		String [] listaResult = new String [lista.length -1];
+		
+		for(int i = 1; i < lista.length; i++) {
+			listaResult[i-1] = lista[i];
+		}
+		return listaResult;
+	}
+	
+	//********************************************************************************************************************************************//
 	
 	//Borrar ultimo elemento de la lista
 	
+	public static String [] eliminarUltimo(String [] lista) {
+		
+		String [] listaResult = new String [lista.length -1];
+		
+		for(int i = 0; i < lista.length-1; i++) {
+			listaResult[i] = lista[i];
+		}
+		return listaResult;
+	}
+	
+	//********************************************************************************************************************************************//
 	
 	//Devolver la media de un array tipo entero (media = suma todo elemntos dividido por el numero de elementos) devuelve dato tipo double
+	
+	public static double media(int [] precios) {
+		
+		double total = 0;
+		
+		for(int i = 0; i < precios.length; i++) {
+			total += precios[i];
+		}
+	
+		total = total/ precios.length;
+		return total;
+	}
 	
 }
 
--- src/bloque_I/Test.java
+++ src/bloque_I/Test.java
@@ -4,8 +4,10 @@ public class Test {
 	
 	public static void main(String [] String) {
 		
-		String [] lista = {"Pepe", "Juan", "Lucas", "Juan, Pepe"};
+		String [] lista = {"Pepe", "Juan", "Lucas"};
 		String valor = "Ana";
+		String valor2 = "Juan";
+		
 		
 		System.out.println("¿Se encuentra el valor en la lista?: " + BibliotecaArray.encontrarString(lista, valor));
 		System.out.println("");
@@ -13,20 +15,44 @@ public class Test {
 		//                  Clase             Metodo      param1  param2
 		String [] lista2 = BibliotecaArray.añadirPrincipio(lista, valor);
 		
-		System.out.print("La nueva lista es: " );
+		System.out.print("La nueva lista al añadir al principio es: " );
 		imprimirArray(lista2);
 		
-		
-		//Era una prueba
-		//System.out.println("¿Se encuentra el valor en la lista?: " + BibliotecaArray.encontrarString(lista2, valor));
-		//System.out.println("");
-		
+
 		
 		//      Clase             Metodo      param1  param2
 		String [] lista3 = BibliotecaArray.añadirFinal(lista, valor);
 
-		System.out.print("La nueva lista es: " );
+		System.out.print("La nueva lista al añadir al final es: " );
 		imprimirArray(lista3);
+		
+		
+		//
+		String [] lista4 = BibliotecaArray.eliminarElemento(lista, valor2);
+
+		System.out.print("La nueva lista al eliminar un elemento es: " );
+		imprimirArray(lista4);
+		
+		
+		//
+		String [] lista5 = BibliotecaArray.eliminarPrimero(lista);
+
+		System.out.print("La nueva lista al eliminar el primero es: " );
+		imprimirArray(lista5);
+		
+		
+		//
+		String [] lista6 = BibliotecaArray.eliminarUltimo(lista);
+
+		System.out.print("La nueva lista al eliminar el ultimo es: " );
+		imprimirArray(lista6);
+		
+		//
+		int [] precios = {21,13,5,30,62};
+		
+		System.out.println("La media es: " + BibliotecaArray.media(precios));
+		System.out.println("");
+		
 		
 	}
 	
