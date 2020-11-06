d0e02b23c8b10bf1115628b2a8203b558e31767e
Tanausu Suarez <tsuarezm@itccanarias.org>
2020-11-06 20:17:16 +0000

    Clase 6-11-20

--- src/bloque_I/BibliotecaArray.java
+++ src/bloque_I/BibliotecaArray.java
@@ -2,7 +2,7 @@ package bloque_I;
 
 public class BibliotecaArray {
 	
-	//Crear un metodo que duevuelva true si el valor pasado por parametro esta en el array
+	//1) Crear un metodo que duevuelva true si el valor pasado por parametro esta en el array
 	
 	public static boolean encontrarString(String [] lista, String valor) {
 		
@@ -16,7 +16,7 @@ public class BibliotecaArray {
 	
 	//********************************************************************************************************************************************//
 	
-	//Crear un metodo pasando por parametro un array de tipo String y un elemento de tipo de String e insertamos el elemento dentro del Array por el principio
+	//2) Crear un metodo pasando por parametro un array de tipo String y un elemento de tipo de String e insertamos el elemento dentro del Array por el principio
 	
 	//lista = {pepe, juan, lucas} tamaño 3
 	//elemento = ana
@@ -36,6 +36,8 @@ public class BibliotecaArray {
 	
 	//********************************************************************************************************************************************//
 	
+	//3) Crear un metodo pasando por parametro un array de tipo String y un elemento de tipo de String e insertamos el elemento dentro del Array por el final
+	
 	//lista = {pepe, juan, lucas} tamaño 3
 	//elemento = ana
 	//listaResult = {null, null, null, null}
@@ -54,7 +56,7 @@ public class BibliotecaArray {
 	
 	//********************************************************************************************************************************************//
 	
-	//Borra un elemento pasado por parametro dentro de un array (array, elemento)
+	//4) Borra un elemento pasado por parametro dentro de un array (array, elemento)
 	
 	//Borrar primer elemento de la lista
 	//          0     1     2
@@ -67,42 +69,36 @@ public class BibliotecaArray {
 	//elemento = ana
 	//listaResult = {null, null, null } tamaño 3
 	
-	
-	
 	public static String [] eliminarElemento(String [] lista, String elemento) {
-		/*
-		int tamaño = 0;
+		
+		String [] listaNueva = new String [lista.length];
+		int j = 0;
+		int element = 0;
 		
 		for(int i = 0; i < lista.length; i++) {
 			if(elemento != lista[i]) {
-				tamaño ++;
+				listaNueva[j] = lista[i];
+				j++;
+				element++;
 				
-			} else if(elemento == lista[i]) {
-				tamaño ++;
 			}
 			
 		}
-		*/
-		//                                  tamaño
-		String [] listaResult = new String [lista.length];
+		if(element == lista.length) return lista;
 		
-		for(int i = 0; i < lista.length; i++) {
-			if(elemento != lista[i]) {
-				listaResult[i]=lista[i];
-				
-			} else if(elemento == lista[i]) {
-				listaResult[i+1]=lista[i];
-			}
-			
+		String [] listaResult = new String [listaNueva.length -1];
+		
+		for(int i = 0; i < listaNueva.length -1; i++) {
+			listaResult[i] = listaNueva[i];
 		}
 		
 		return listaResult;
-		
 	}
-	
+		
 	//********************************************************************************************************************************************//
 	
-	//Borrar primer elemento de la lista
+	//5) Borrar primer elemento de la lista
+	
 	//          0     1     2
 	//lista = {pepe, juan, lucas} tamaño 3
 	//                0     1
@@ -120,7 +116,7 @@ public class BibliotecaArray {
 	
 	//********************************************************************************************************************************************//
 	
-	//Borrar ultimo elemento de la lista
+	//6) Borrar ultimo elemento de la lista
 	
 	public static String [] eliminarUltimo(String [] lista) {
 		
@@ -134,7 +130,7 @@ public class BibliotecaArray {
 	
 	//********************************************************************************************************************************************//
 	
-	//Devolver la media de un array tipo entero (media = suma todo elemntos dividido por el numero de elementos) devuelve dato tipo double
+	//7)Devolver la media de un array tipo entero (media = suma todo elemntos dividido por el numero de elementos) devuelve dato tipo double
 	
 	public static double media(int [] precios) {
 		
@@ -144,47 +140,270 @@ public class BibliotecaArray {
 			total += precios[i];
 		}
 	
-		total = total/ precios.length;
+		total = total / precios.length;
 		return total;
 	}
 	
 	/*EJERCICIOS PROPUESTO*/
 	
 	/*
-	 * crea un método que elimine un elemento de una posición dada de un array
+	 * 8) crea un método que elimine un elemento de una posición dada de un array
 	 * */
+	// 0,1,2,3,4,5
+	//{2,4,6,7,1,9} tam 6
+	//pos 3
+	//{2,4,6,0,1,9}
+	//{2,4,6,1,9,0}
+	//{2,4,6,1,9}
+	
+	public static int [] eliminarPos (int [] array, int pos) {
+		
+		int [] arrayPrimero = new int [array.length];
+		int j = 0;
+		
+		for(int i = 0; i < array.length; i++) {	
+			if(pos != i) {
+				arrayPrimero[j] = array[i];
+				j++;
+			}		
+		}
+		
+		int [] arraySegundo = new int [arrayPrimero.length-1];
+		
+		for(int i = 0; i < arrayPrimero.length -1; i++) {
+			arraySegundo[i] = arrayPrimero[i];
+		}
+		if(pos>array.length) return array;
+		return arraySegundo;
+	}
 	
 	/*
-	 * Crea un método que ordene los elemento de un array. Tendrá un parámetro boolean, si este es true, lo ordenará de mayor a menor, si es falso
+	 * 9) Crea un método que ordene los elemento de un array. Tendrá un parámetro boolean, si este es true, lo ordenará de mayor a menor, si es falso
 	 * de menor a mayor
 	 * */
 	
-	/*
-	 * crea un metodo que guarde dentro de un array un elemento en la posición que se le indica. Si la izquierda no hay valores se rellena con 0
-	 * */
+	public static int [] ordenarElemento(int [] array, boolean param1) {
+		
+		if(param1) {
+			return generar1(array);
+		}
+		return generar2(array) ;
+	}
+	
+	private static int [] generar1(int [] array) {
+		
+		int temp = 0;
+		
+		for (int i = 0; i < array.length-1; i++){
+			
+			for (int j = i+1; j < array.length; j++) {
+				
+				if (array[i] > array[j]) {					
+					temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
+				}
+			}
+		}
+		
+		return array;
+	}
+	
+	private static int [] generar2(int [] array) {
+		
+		int temp = 0;
+		
+		for (int i = 0; i < array.length-1; i++){
+			
+			for (int j = i+1; j < array.length; j++) {
+				
+				if (array[i] < array[j]) {					
+					temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
+				}
+			}
+		}
+		
+		return array;
+	}
 	
 	/*
-	 * Crea un método que devuelva las letras del DNI. Las letras del DNI  se obtiene como el resto de dividir el número del DNI entre 23,
+	 * 10) crea un metodo que guarde dentro de un array un elemento en la posición que se le indica. Si la izquierda no hay valores se rellena con 0
+	 * */
+	
+	//array = { , , , , }
+	//pos = 2 guarde numero
+	//numero = 7
+	//array = { , ,7, , }
+	//array = {0,0,7}
+	
+	public static int [] gurdarDentro(int [] guardado, int numero,  int pos) {
+		
+		if(pos < guardado.length) {
+			guardado[pos] = numero;
+			
+			return guardado;
+		}	
+		
+		int [] guardadoNuevo = new int[pos+1];
+		guardadoNuevo[pos] = numero;
+		
+		for(int i = 0; i < guardado.length; i++) {
+			guardadoNuevo[i] = guardado[i];
+		}
+		
+		return guardadoNuevo;
+	}
+	
+	/*
+	 * 11)Crea un método que devuelva las letras del DNI. Las letras del DNI  se obtiene como el resto de dividir el número del DNI entre 23,
 	 * Por ejemplo, si introducimos el  DNI 20267079, el resto de dividirlo por 23 sería 8, luego la letra sería la P  
 	 * 
 	 * Posición     0   1   2   3	4	5   6	7	8	9	10	11  12	13  14  15  16  17  18  19  20  21  22
 	 * Letra        T   R  	W 	A	G	M	Y	F	P	D	X	B	N   J    Z   S   Q   V   H   L   C   K   E
 	 * */
 	
-	/*
-	 * Crea un método que compare dos array y que devuelva true si son iguales o false si no lo son
-	 * */
+	public static String letras(String [] posLetra, int dni) {
+		
+		String devuelve = "";
+		int resto = dni%23;
+		
+		devuelve = posLetra[resto];
+		
+		return devuelve;
+	}
 	
 	/*
-	 * Crea un método que devuelva una sub-array de un array dado. El método recibirá además dos parámetros, que determine el principio y el fin de los valores que
-	 * debe devolver el método
+	 * 12) Crea un método que compare dos array y que devuelva true si son iguales o false si no lo son
+	 * */
+	
+	//{"hola", "mundo"};
+	//{"hola", "mundo"}
+	//{"hola", "juan"}
+	//{"mundo", "hola",};
+	//{"hola", "a", "todos"};
+	//{"hola"};
+	
+	public static boolean comparar(String [] param1, String [] param2) {
+		
+		if(param1.length != param2.length) return false;
+		
+		for(int i = 0; i < param1.length; i++) {
+			if(param1[i] != param2[i]) {
+				return false;
+			}
+		}
+		return true;
+		
+	}
+	
+	/*
+	 * 13) Crea un método que devuelva una sub-array de un array dado. El método recibirá además dos parámetros, que determine el principio y 
+	 * el fin de los valores que debe devolver el método
 	 * Por ejemplo:
-	 *   array = [2, 3, 4, 5, 6, 8] ini = 1, fin = 3
+	 *   array = [2, 3, 4, 5, 6, 8] tam = 6     ini = 1, fin = 3
 	 *   array resultado = [3, 4, 5]
 	 */
 	 
-	
+	public static int [] arrayDado(int [] array, int ini, int fin) {
+		
+		//Si array =0  devoolver vacio
+		if(array.length == 0) {
+			return new int [0];
+		}
+		//ini > array devolver vacio
+		if(ini >= array.length) {
+			return new int [0];
+		}
+		//fin< array devolver vacio
+		if(fin < 0) {
+			return new int [0];
+		}
+		//ini > fin devolver vacio
+		if(ini > fin) {
+			return new int [0];
+		}
+		//ini dentro del array pero fin fuera de el nos devuleve desde ini hasta el final del array
+		//Si ini = fin 
+		if(ini == fin) {
+			int [] arrayTam = new int [1];
+			arrayTam[0] =array[ini];
+			return arrayTam;
+		}
+		
+		
+		//Contador para saber el tamaño de lo que extraeremos en el subArray
+		int contador = 0;
+		//ini=2 fin=4
+		//ite 1 i = 2; cont = 0
+		//ite 2 i = 3; cont = 1
+		//ite 3 i = 4; cont = 2 
+		
+		
+		for(int i = ini; i < fin+1; i++) {
+			contador ++;
+		}
+		
+		//arrayCom = {2, 3, 4, 5, 6, 8};
+		//ini = 1;
+		//fin = 3;
+		//suArray ={ , , }
+		
+		if(ini < array.length && ini >= 0) {
+			int [] suArray = new int [contador];
+			int j = 0;
+			
+			for(int i = ini; i < suArray.length && i < array.length; i++) {
+				suArray[j] = array[i];
+				j++;
+			}
+			return suArray;
+		}
+		return array;
+		/*
+		
+		int [] arrayVacio = new int[0];	
+		Si inicia fuera de una posicion que no tiene el array, devuelve un array vacio del tamaño entre ini y fin
+		Ej: array = {2,4,6}| ini=5 y fin=6 | Devuelve subArray = {0,0}
+		Pero si ini=0 y fin=3 deberia de devolver subArray = {2,4,6,0}
+		
+		for(int i = 0; i < array.length; i++) {
+			if(ini > array[i] && fin > array[i]) { //A la hora de hacer la condicion confundi || y &&
+				return arrayVacio;	
+			}
+		}
+		int [] subArray = new int[contador];
+		
+		//Si empieza en 0
+		if(param1 == 0){
+			for(int i = param1; i < param2+1; i++) {
+				subArray[i]=array[i];
+			}
+			return subArray;
+		}
+		
+		//Si empieza en 1
+		if(param1 == 1){
+			for(int i = param1; i < param2+1; i++) {
+				subArray[i-1]=array[i];
+			}
+			return subArray;
+		}
+		
+		//Si empieza en 2
+		if(param1 == 2){
+			for(int i = param1; i < param2+1; i++) {
+				subArray[i-2]=array[i];
+			}
+			return subArray;
+		}
+		return subArray;
+		*/
+		
+	}
 }
 
-
-
+		//arrayCom = {2, 3, 4, 5, 6, 8};
+		//ini = 1;
+		//fin = 5;
---
+++ src/bloque_I/BibliotecaArrayII.java
@@ -0,0 +1,165 @@
+/**
+ * La clase biblioteca Array II esta destinada a la explicación y realización de ejercicios con array bidimencionales
+ */
+package bloque_I;
+
+/**
+ * @author Tanausú suárez
+ *
+ */
+public class BibliotecaArrayII {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/*
+		int[] valores;
+		valores = new int [5];
+		//[0,0,0,0,0]
+		int[] valores1 = new int[5];
+		valores = {0,0,0....,0}
+		*/
+		int[][] valores;
+		valores = new int[2][3];
+		
+		int[][] valores1  = new int[2][3];
+		/*	  0 1 2 = j
+		 * 0 [0,0,0]
+		 * 1 [0,0,0]
+		 * =
+		 * i		
+		 */
+		//     0		1			valores.length-1
+		// [[0,0,0], [0,0,0], ..., [0,0,0,0]]
+		//[0, 1, 4, 2, 3] = 5
+		//[[],[],[]] = 3
+		
+		
+		valores[0][0] = 1;
+		valores[0][1] = 2;
+		valores[1][0] = 4;
+		valores[1][1] = 5;
+
+		
+		
+		int[]resultado = sumaArrayFila(valores);
+		/*
+		for (int j = 0; j < resultado.length;j++) {
+			System.out.print(resultado[j] + ", ");
+		}
+		*/
+		
+		resultado = sumaArrayColumna(valores);
+		for (int j = 0; j < resultado.length;j++) {
+			System.out.print(resultado[j] + ", ");
+		}
+
+
+	}
+	private static void printlnArray(int[][] valor){
+		for (int i = 0; i < valor.length; i++){
+			System.out.print("[");
+			for (int j = 0; j < valor[i].length;j++) {
+				System.out.print(valor[i][j] + ", ");
+			}
+			System.out.print("]\n");
+		}
+	}
+	
+	/*
+	 * CREAR UN MÉTODO QUE HAGA LA SUMA POR FILA DE UNA MATRIZ 
+	 * EJEMPLO:
+	 * Argumento:
+	 * 0 [1,2,3] 
+	 * 1 [6,5,4]
+	 * Resultado.
+	 * [6, 15]
+	 * */
+	/*	  0 1 2 = j
+	 * 0 [0,0,0]
+	 * 1 [0,0,0]
+	 * =
+	 * i		
+	 */
+	private static int[] sumaArrayFila(int[][] valores){
+		
+		int[] resultado = new int[valores.length];
+		int sumatoria;
+		
+		for (int i = 0; i < valores.length ;i++) {
+			sumatoria = 0;
+			for (int j = 0; j < valores[i].length ;j++) {
+				sumatoria = sumatoria + valores[i][j];
+			}
+			resultado[i] = sumatoria;
+		}
+		
+		return resultado;
+	}
+	/*
+	 * CREAR UN MÉTODO QUE HAGA LA SUMA POR COLUMNAS DE UNA MATRIZ 
+	 * EJEMPLO:
+	 * Argumento:
+	 * 0 [1,2,3] 
+	 * 1 [6,2,5]
+	 * Resultado.
+	 * [7, 4, 8]
+	 * */
+	/*	  0 1 2 = j
+	 * 0 [0,0,0]
+	 * 1 [0,0,0]
+	 * =
+	 * i		
+	 */
+	
+	private static int[] sumaArrayColumna(int[][] valores){
+		
+		if (valores.length == 0) return new int[0];
+			
+		int[] resultado = new int[valores[0].length];
+		int sumatoria;
+		
+		for (int i = 0; i < valores[0].length ;i++) {
+			sumatoria = 0;
+			for (int j = 0; j < valores.length ;j++) {
+				sumatoria = sumatoria + valores[j][i];
+			}
+			resultado[i] = sumatoria;
+		}
+		
+		
+		return resultado;
+		
+	}
+	
+	/*
+	 * Segun u na matrix contar el número de elemento que se repiten en esa posicioón
+	 * ejempñlo:
+	 * 
+	 * Argumento
+	 * [1, 2, 2, 4]
+	 * [2, 3, 5, 2]
+	 * [1, 6, 0, 2]
+	 * 
+	 * 
+	 * resultado:
+	 * [1, 4, 4, 0]
+	 * [4, 0, 0, 4]
+	 * [1, 0, 0, 4]
+	 * 
+	 * */
+
+	
+	/*
+	 * Argumento: realizar la siguiente tabla 7
+	 * [2 , 1, 2]
+	 * [2 , 2, 4]
+	 * [2, 3, 6]
+	 * [2, 4, 8]
+	 * 
+	 */
+	
+	
+	 
+}
